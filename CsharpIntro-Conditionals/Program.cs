dd42aa66839550fbf2a9282cd876ec7088155c23
Raf Ceuls <rcl@foreach.be>
2018-09-14 16:00:21 +0200

    initial course data

---
+++ CsharpIntro-Conditionals/Program.cs
@@ -0,0 +1,40 @@
+﻿
+using System;
+
+namespace CSharpIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var check = true;
+            if (check)
+            {
+                Console.WriteLine("Item 1");
+            }
+            else
+            {
+                Console.WriteLine("Item 2");
+            }
+
+            if (!check)
+            {
+                Console.WriteLine("Item 3");
+            }
+            else
+            {
+                Console.WriteLine("Item 4");
+            }
+
+            if (1 == 1 + 2)
+            {
+                Console.WriteLine("Item 5");
+            }
+
+            if (1 == 2 - 1)
+            {
+                Console.WriteLine("Item 6");
+            }
+        }
+    }
+}
---
+++ CsharpIntro-Variables/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CSharpIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hello = "Hello World with a variable!"; // string
+            Console.WriteLine(hello);
+
+            var number1 = 1; // integer 
+            var number2 = 2.0; // double
+            var number3 = 2; // integer
+            Console.WriteLine(number1 / number2);
+            Console.WriteLine(number1 / number3);
+
+            var booll = true; // boolean (true of false)
+
+            Console.WriteLine(booll);
+
+            bool? nullable = null; // nullable
+            Console.WriteLine(nullable);
+            nullable = true;
+            Console.WriteLine(nullable);
+
+            // nullable = 1; // werkt niet, is een int in een nullable bool!
+        }
+    }
+}
