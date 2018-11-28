bdfb3bbfebff969ae63be253ae1bd61d2ed11ca5
Raf Ceuls <rceuls@gmail.com>
2018-11-28 14:39:19 +0100

    added nlog

--- CSharp-Pline/Controllers/PokeActionFilter.cs
+++ CSharp-Pline/Controllers/PokeActionFilter.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Logging;
 using System.Reflection;
 
 namespace CSharp_Pline.Controllers
@@ -7,12 +8,14 @@ namespace CSharp_Pline.Controllers
   {
     public override void OnActionExecuting(ActionExecutingContext context)
     {
-      System.Console.WriteLine($"TEST 123 {context.Controller.GetType().Name} - {context.ActionDescriptor.DisplayName}");
+      ILogger<PokeActionFilter> log = (ILogger<PokeActionFilter>)context.HttpContext.RequestServices.GetService(typeof(ILogger<PokeActionFilter>));
+      log.LogInformation($"{context.Controller.GetType().Name} - {context.ActionDescriptor.DisplayName}");
     }
 
     public override void OnActionExecuted(ActionExecutedContext context)
     {
-      System.Console.WriteLine($"TEST 456 {context.Controller.GetType().Name} - {context.ActionDescriptor.DisplayName}");
+      ILogger<PokeActionFilter> log = (ILogger<PokeActionFilter>)context.HttpContext.RequestServices.GetService(typeof(ILogger<PokeActionFilter>));
+      log.LogInformation($"{context.Controller.GetType().Name} - {context.ActionDescriptor.DisplayName}");
     }
   }
 }
--- CSharp-Pline/Program.cs
+++ CSharp-Pline/Program.cs
@@ -3,22 +3,47 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using CSharp_Pline;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using NLog.Web;
 
-namespace CSharp_Pline
+namespace CSharp_Plined
 {
-    public class Program
+  public class Program
+  {
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            CreateWebHostBuilder(args).Build().Run();
-        }
-
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+      var logger = NLog.Web.NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+      try
+      {
+        logger.Debug("init main");
+        BuildWebHost(args).Run();
+      }
+      catch (Exception ex)
+      {
+        //NLog: catch setup errors
+        logger.Error(ex, "Stopped program because of exception");
+        throw;
+      }
+      finally
+      {
+        // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+        NLog.LogManager.Shutdown();
+      }
     }
+
+    public static IWebHost BuildWebHost(string[] args) =>
+        WebHost.CreateDefaultBuilder(args)
+        .UseStartup<Startup>()
+        .ConfigureLogging(logging =>
+        {
+          logging.ClearProviders();
+          logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+        })
+        .UseNLog()  // NLog: setup NLog for Dependency injection
+        .Build();
+  }
 }
